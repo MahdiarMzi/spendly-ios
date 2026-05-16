302b0d46dd782e363260136136b4594a48317891
mahdiar <mahdiar.mazinani@gmail.com>
2026-05-16 12:42:33 -0700

    Initial Commit

---
+++ Spendly/SpendlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendlyApp.swift
+//  Spendly
+//
+//  Created by Mahdiar Mazinani on 2026-05-16.
+//
+
+import SwiftUI
+
+@main
+struct SpendlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
